52a702a11d5a0afca51c7036a1a98889137dc7d8
Anton Fedosov <fed.guman@gmail.com>
2020-08-22 01:00:08 +0300

    cross border tab movement support

--- vimtabs.js
+++ vimtabs.js
@@ -22,9 +22,16 @@ var moveTab = function (direction, tabs) {
     var pinnedCount = tabs.filter(tab => tab.pinned).length;
     var left = activeTab.pinned ? 0 : pinnedCount;
     var right = (activeTab.pinned ? pinnedCount : tabs.length) - 1;
-    var step = 1;
-    if (direction === "left") step = -step
-    chrome.tabs.move(activeTab.id, {index: Math.max(left, Math.min(right, activeTab.index + step))});
+    var idx = (() => {
+        switch (direction) {
+        case "right":
+          return activeTab.index === right ? left : activeTab.index + 1;
+        case "left":
+          return activeTab.index === left ? right : activeTab.index - 1;
+      }
+    })();
+  chrome.tabs.move(activeTab.id, { index: idx })
+
 }
 chrome.commands.onCommand.addListener(function (command) {
   chrome.tabs.query({ currentWindow: true }, BackgroundCommands[command]);
